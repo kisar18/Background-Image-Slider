01abadc5b5b4913fb7f2e8a64f90e5d4259267cd
kisar18 <martinpur99@seznam.cz>
2020-08-21 19:17:52 +0200

    Slider

---
+++ script.js
@@ -0,0 +1,31 @@
+var lBtn = document.getElementById('btn-l')
+var rBtn = document.getElementById('btn-r')
+var cont = document.getElementsByClassName('container')[0]
+
+var value = 0
+
+function checkValue() {
+    if (value < 0) {
+        value = 4
+    }
+    else if (value > 4) {
+        value = 0
+    }
+    else {
+        value += 0
+    }
+}
+
+var images = ["contBcg-0.jpeg", "contBcg-1.jpeg", "contBcg-2.jpeg", "contBcg-3.jpeg", "contBcg-4.jpeg"]
+
+lBtn.addEventListener('click', () => {
+    value -= 1
+    checkValue()
+    cont.style.background = "url(/images/" + images[value] + ") center/cover no-repeat"
+
+})
+rBtn.addEventListener('click', () => {
+    value += 1
+    checkValue()
+    cont.style.background = "url(/images/" + images[value] + ") center/cover no-repeat"
+})
